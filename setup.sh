2b3ab23bea05d7743e9220c3a3e8b4d0704df5db
Intelligent UAS <61607236+QuetzalCpp@users.noreply.github.com>
2022-05-06 20:09:39 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 cp -r ./gazebo_models/* ~/.gazebo/models/
 rm -r gazebo_models
-cp -r ./launch/* ~/bebop_ws/src/iROS_drone/rotors_simulator/rotors_gazebo/launch
+cp -r ./launch ~/bebop_ws/src/iROS_drone/rotors_simulator/rotors_gazebo/launch
 rm -r launch
-cp -r ./race_tracks/* ~/bebop_ws/src/iROS_drone/rotors_simulator/rotors_gazebo/
-rm -r race_tracks
+cp -r ./adr_worlds ~/bebop_ws/src/iROS_drone/rotors_simulator/rotors_gazebo/
+rm -r adr_worlds
 
