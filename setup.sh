048069572a6617e8e4fa27c499e3dfe93e30fce6
Intelligent UAS <61607236+QuetzalCpp@users.noreply.github.com>
2022-05-06 22:53:49 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,8 @@ rm -r launch
 cp -r ./adr_worlds ~/bebop_ws/src/iROS_drone/rotors_simulator/rotors_gazebo/
 rm -r adr_worlds
 
+cp -r ./keyboard ~/bebop_ws/src/
+rm -r keyboard
+
 sudo apt install python3-pip
 pip install -r requirements.txt
